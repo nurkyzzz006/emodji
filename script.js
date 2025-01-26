1745eaf68f69eb360a21a2abd9db3377ac3d7a90
nurkyzzz006 <nasnur399@gmail.com>
2025-01-26 20:40:10 +0600

    .

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let burger = document.querySelector(".burger");
 let x = document.querySelector(".x");
 burger.addEventListener("click", () => {
   list.style.display = "block";
+  list.style.flex_direction = "column"
 });
 // list.addEventListener("click", () => {
 
